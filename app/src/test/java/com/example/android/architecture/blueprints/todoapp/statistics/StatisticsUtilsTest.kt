9e417bf9ceae53a602a5da580d22364dfe1926fa
Aditya Gupta <52538173+aadityaguptaa@users.noreply.github.com>
2021-09-06 02:02:03 +0530

    first commit

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,11 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import org.junit.Assert.*
+
+class StatisticsUtilsTest{
+
+    //If there's no completed task and one active task,
+    // then there are 100% percent active tasks and 0% completed tasks.
+
+
+}
